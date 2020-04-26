e06a628a7fe3ff9f1fd23279824cc06df5ed8208
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-04-26 22:36:41 +0200

    Improved entities.

--- file_indexer_api/common/connection_handler.py
+++ file_indexer_api/common/connection_handler.py
@@ -1,8 +1,8 @@
 from rpyc import connect
 
 class ConnectionHandler(object):
-    connection = connect('localhost', 18861)
-    root = None
+  connection = connect('localhost', 18861)
+  root = None
 
-    def get_handler(self):
-        return self.root
+  def get_handler(self):
+    return self.root
--- file_indexer_api/common/searcher_handler.py
+++ file_indexer_api/common/searcher_handler.py
@@ -1,4 +1,4 @@
 from file_indexer_api.common.connection_handler import ConnectionHandler
 
 class SearcherHandler(ConnectionHandler):
-    root = ConnectionHandler.root.searcher()
+  root = ConnectionHandler.root.searcher()
