d3eee622ba88ae5bda2edfa2ae57454d2201e619
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-04-29 00:00:35 +0200

    Connected to Java and Middleman completely.

--- file_indexer_api/entities/indexer.py
+++ file_indexer_api/entities/indexer.py
@@ -1,6 +1,6 @@
 from file_indexer_api.common.connection_handler_factory import ConnectionHandlerFactory
 
-class Indexer():
+class Indexer:
   def __init__(self):
     self.indexer = ConnectionHandlerFactory.create_connection_handler('indexer')
 
