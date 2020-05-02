8a72176aabdaac2b301e75e6d91d7a75fc28f7f6
Diego Francisco Hernandez Guevara <34254918+DiegoFHG@users.noreply.github.com>
2020-05-02 18:24:43 +0200

    Update indexer_controller.py

--- file_indexer_api/controllers/indexer_controller.py
+++ file_indexer_api/controllers/indexer_controller.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from rpyc import connect
 
 parser = reqparse.RequestParser()
 parser.add_argument('path')
@@ -6,5 +7,10 @@ parser.add_argument('path')
 class IndexerController(Resource):
   def post(self):
     args = parser.parse_args()
+    connection = connect('localhost', 18861)
+    result = connection.root.index(args['path'])
 
-    return {'path': args['path']}
+    if (result is True):
+      return {'message': 'indexed path {}'.format(args['path'])}
+
+    return {'message': 'could no index path {}'.format(args['path'])}
